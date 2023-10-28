79595fef87a23f586a96cbeb6d42f8e714f1a695
Dicainum <mentokcnn@gmail.com>
2023-10-28 15:18:55 +0300

    task "dialog"

--- src/dialog.js
+++ src/dialog.js
@@ -1,4 +1,5 @@
-console.log('Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"');
-console.log('Walter White: "What is it?"');
-console.log('Jesse Pinkman: "It\'s a giant magnet!"');
-console.log('Walter White: "What the hell are we going to do with a giant magnet?"');
+const q = 'Jesse Pinkman: "Yo, Mr. White! I\'ve got a surprise for you!"\n';
+const w = 'Walter White: "What is it?"\n';
+const e = 'Jesse Pinkman: "It\'s a giant magnet!"\n';
+const r = 'Walter White: "What the hell are we going to do with a giant magnet?"';
+console.log(q + w + e + r);
